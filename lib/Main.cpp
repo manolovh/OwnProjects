cca6a43d8675df2a9fee72ae5e4d2c75457d0445
Harry <h.manolov77@gmail.com>
2023-07-30 10:35:08 +0300

    Setup done

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Header.hpp"
+#include "../inc/Header.hpp"
 
 int main()
 {
@@ -8,5 +8,5 @@ int main()
 	person_data.age = 18;
 	person_data.weight = 85;
 
-	std::cout << "AGE: " << person_data.age << " WEIGHT: " << person_data.weight;
+	std::cout << "AGE: " << person_data.age << " WEIGHT: " << person_data.weight << std::endl;
 }
