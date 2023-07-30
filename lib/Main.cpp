b40e23e5d0f25107b348aa0a6def75d1f79e4be6
Harry <h.manolov77@gmail.com>
2023-07-30 19:48:14 +0300

    Initial print

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -1,12 +1,23 @@
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <vector>
 #include "../inc/Header.hpp"
+
+std::string name = std::filesystem::current_path();
+std::vector<std::filesystem::__cxx11::path> files;
+std::vector<std::filesystem::__cxx11::path> directories;
 
 int main()
 {
-	Data person_data;
-
-	person_data.age = 18;
-	person_data.weight = 85;
-
-	std::cout << "AGE: " << person_data.age << " WEIGHT: " << person_data.weight << std::endl;
+	for (const auto& entry: std::filesystem::directory_iterator(name))
+	{
+		if (entry.is_directory())
+		{
+			directories.push_back(entry.path().filename().concat("/").c_str());
+		}
+	}
+	int i = 1;
+	for (auto elem: directories)
+		std::cout << std::setw(5) << std::left << i++ << elem.c_str() << std::endl;
 }
