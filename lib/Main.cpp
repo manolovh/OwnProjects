053742ecec2ce21fb5b99a895d529506cb763d3b
Harry <h.manolov77@gmail.com>
2023-08-02 21:22:14 +0300

    Part 2 of App

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -4,20 +4,75 @@
 #include <vector>
 #include "../inc/Header.hpp"
 
-std::string name = std::filesystem::current_path();
-std::vector<std::filesystem::__cxx11::path> files;
-std::vector<std::filesystem::__cxx11::path> directories;
+using path_t = std::vector<std::filesystem::__cxx11::path>;
 
-int main()
+struct Tree
 {
-	for (const auto& entry: std::filesystem::directory_iterator(name))
+	std::string m_name;
+	std::vector<Tree> children;
+
+	Tree(std::string name) : m_name{name} {}
+};
+
+void print_directory(const Tree& directory)
+{
+	std::cout << directory.m_name << std::endl;
+	for (auto elem: directory.children)
 	{
-		if (entry.is_directory())
+		if (dynamic_cast<Tree*>(&elem.children[0]) == nullptr)
 		{
-			directories.push_back(entry.path().filename().concat("/").c_str());
+			print_directory(elem);
+		}
+		else
+		{
+			std::cout << elem.m_name << std::endl;
 		}
 	}
-	int i = 1;
-	for (auto elem: directories)
-		std::cout << std::setw(5) << std::left << i++ << elem.c_str() << std::endl;
+}
+
+void collect_data(Tree& current_dir)
+{
+	static std::string elem_name;
+	static std::string previous_dir;
+	for (const auto& entry: std::filesystem::directory_iterator(current_dir.m_name))
+	{
+		// Add all directories that are not hidden
+		if (entry.is_directory() && (entry.path().filename().c_str()[0] != '.'))
+		{
+			elem_name += entry.path().filename().concat("/").c_str();
+			current_dir.children.emplace_back(Tree(elem_name));
+
+			previous_dir += current_dir.m_name + "/";
+			collect_data(current_dir.children[current_dir.children.size() - 1]);
+
+			elem_name = previous_dir;
+		}
+		// Add all files that are not hidden
+		else if (entry.is_regular_file() && (entry.path().filename().c_str()[0] != '.'))
+		{
+			elem_name = entry.path().filename();
+			current_dir.children.push_back(elem_name);
+		}
+		// Reset current directory name
+		else
+		{
+			elem_name = previous_dir;
+		}
+	}
+}
+
+std::string name = std::filesystem::current_path();
+
+int main(int argc, char *argv[])
+{
+	if (argv[1])
+	{
+		name += '/';
+		name += argv[1];
+	}
+
+	Tree current_dir = Tree(name);
+
+	collect_data(current_dir);
+	print_directory(current_dir);
 }
